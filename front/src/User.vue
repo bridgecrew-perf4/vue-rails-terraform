a1c6f5ab3840a607b26cd6f8358caf517a1c7a71
naokivandit <n.i.vandit0924@gmail.com>
2021-01-18 02:55:50 +0900

    add user vue

---
+++ front/src/User.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ user.name}}</h1>
+    <table>
+      <td>{{ user.id }}</td>
+      <td>{{ user.name }}</td>
+      <td>{{ user.email }}</td>
+      <td>{{ user.age }}</td>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        user: null
+      };
+    },
+    mounted() {
+      this.axios
+        .get(`http://localhost:3000/users/${this.$route.params['id']}`)
+        .then(response => (this.user = response.data))
+    }
+  }
+</script>
