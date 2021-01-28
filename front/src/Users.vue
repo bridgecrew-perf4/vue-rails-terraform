841e59737f4d5c02cf8e426c7799b1ad8c399d2e
naokivandit <n.i.vandit0924@gmail.com>
2021-01-29 02:07:16 +0900

    fix: user vue

--- front/src/Users.vue
+++ front/src/Users.vue
@@ -1,32 +1,65 @@
 <template>
-  <div>
-    <h1>ユーザー一覧</h1>
-    <table>
-      <tr v-for="user in users" :key="user.id">
-        <td>
-          <router-link :to="{ name: 'users', params: { userID: user.id } }">
-            <p>{{ user.id }}</p>
-          </router-link>
-        </td>
-        <td>{{ user.name }}</td>
-        <td>{{ user.email }}</td>
-        <td>{{ user.age }}</td>
-      </tr>
-    </table>
-  </div>
+  <v-container text-xs-center justify-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h1>ユーザー一覧</h1>
+      </v-flex>
+
+      <v-flex xs12 mt-5 mr-5 text-right>
+        <router-link :to="{ name: 'userCreate' }">
+          <v-btn color="info">
+            ユーザー追加
+          </v-btn>
+        </router-link>
+      </v-flex>
+
+      <v-flex xs12 mt-3 justify-center>
+        <v-data-table :headers="headers" :items="users">
+          <template v-slot:[`item.action`]="{ item }">
+            <router-link :to="{ name: 'user', params: { id: item.id } }">
+              <v-icon small class="mr-2">mdi-pencil</v-icon>
+            </router-link>
+            <v-icon small class="mr-2" @click="deleteUser(item.id)"
+              >mdi-delete</v-icon
+            >
+          </template>
+        </v-data-table>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      users: null
+      headers: [
+        { text: "ID", value: "id" },
+        { text: "名前", value: "name" },
+        { text: "メールアドレス", value: "email" },
+        { text: "年齢", value: "age" },
+        { text: "操作", value: "action", sortable: false }
+      ],
+      users: []
     };
   },
   mounted() {
     this.axios
       .get("http://localhost:3000/users")
-      .then(response => (this.users = response.data))
+      .then(response => (this.users = response.data));
+  },
+  methods: {
+    deleteUser(id) {
+      this.axios.delete(`http://localhost:3000/users/${id}`);
+      alert("ユーザー情報を削除しました");
+      location.reload(); // storeに置き換える
+    }
   }
 };
 </script>
+
+<style scoped lang="scss">
+a {
+  text-decoration: none;
+}
+</style>
