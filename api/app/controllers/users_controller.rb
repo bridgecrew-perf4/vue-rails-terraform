2d69423b398c229c565a4b94af5533fa008382b1
naokivandit <n.i.vandit0924@gmail.com>
2021-01-28 22:29:45 +0900

    feature: user controller

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -1,11 +1,51 @@
 class UsersController < ApplicationController
+  # POST users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: { status: 'success', data: @user }
+    else
+      render json: { status: 'error', data: @user.errors }
+    end
+  end
+
+  # GET users/:id
   def index
     @users = User.all
     render json: @users
   end
 
+  # GET users
   def show
     @user = User.find(params[:id])
     render json: @user
   end
+
+  # PUT users/:id
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      render json: { status: 'success', data: @user }
+    else
+      render json: { status: 'error', data: @user.errors }
+    end
+  end
+
+  # DELETE users/:id
+  def destroy
+    @user = User.find(params[:id])
+
+    if @user.destroy
+      render json: { status: 'success' }
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :age)
+  end
+
 end
