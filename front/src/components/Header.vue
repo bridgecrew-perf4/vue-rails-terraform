48381a1e831ba038cc61d93ea3f5a4133b1476d8
naokivandit <n.i.vandit0924@gmail.com>
2021-01-29 02:05:31 +0900

    fix: header vue

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,16 +1,10 @@
 <template>
-  <v-app>
-    <v-app-bar app color="orange" dark>
-      <v-toolbar-title>Sample app</v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-app-bar>
-
-    <v-main>
-      <v-container fluid fill-height align-start>
-        <router-view />
-      </v-container>
-    </v-main>
-  </v-app>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">
+      Sample App
+    </div>
+    <v-spacer></v-spacer>
+  </v-app-bar>
 </template>
 
 <script></script>
