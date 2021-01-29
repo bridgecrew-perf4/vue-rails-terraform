33299ae1f751739afb1fa4129bd63d15e31be0c2
naokivandit <n.i.vandit0924@gmail.com>
2021-01-29 19:25:06 +0900

    fix and mv home vue

---
+++ front/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container text-center justify-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h1>食事・運動ログアプリ</h1>
+        <p>
+          食事・運動ログアプリをご利用の方は、Googleアカウントでログインしてください。
+        </p>
+      </v-flex>
+
+      <v-flex xs12 mt-5>
+        <v-btn color="info">Googleアカウントでログイン</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+</script>
