cbf9458d18cbe913c110191a78b8280df1f622a3
naokivandit <n.i.vandit0924@gmail.com>
2021-01-31 15:29:31 +0900

    fix: footer vue

--- front/src/components/Footer.vue
+++ front/src/components/Footer.vue
@@ -2,7 +2,7 @@
   <v-footer color="primary lighten-1" padless>
     <v-row justify="center" no-gutters>
       <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
-        {{ new Date().getFullYear() }} — <strong>Sample App</strong>
+        {{ new Date().getFullYear() }} — <strong>食事・運動ログアプリ</strong>
       </v-col>
     </v-row>
   </v-footer>
