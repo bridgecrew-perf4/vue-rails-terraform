9e4a974c1d4d08603807249f824d782f49397835
naokivandit <n.i.vandit0924@gmail.com>
2021-01-31 15:31:47 +0900

    fix header vue

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,7 +1,8 @@
 <template>
   <v-app-bar app color="primary" dark>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
     <div class="d-flex align-center">
-      Sample App
+      食事・運動ログアプリ
     </div>
     <v-spacer></v-spacer>
   </v-app-bar>
