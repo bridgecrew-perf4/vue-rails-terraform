da551404f044fc0508973c0b10b1ac3ead071f21
naokivandit <n.i.vandit0924@gmail.com>
2021-02-03 03:16:40 +0900

    feature: add atoms

---
+++ front/src/components/atoms/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-btn color="info">
+    {{ text }}
+  </v-btn>
+</template>
+
+<script lang="ts">
+export default {
+  props: ["text"]
+};
+</script>
+
+<style lang="scss" scoped>
+a {
+  text-decoration: none;
+}
+</style>
---
+++ front/src/components/atoms/Table.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-data-table :headers="headers" :items="records">
+    <template v-slot:[`item.action`]="{ item }">
+      <router-link :to="{ name: editPage, params: { id: item.id } }">
+        <v-icon small class="mr-2">mdi-pencil</v-icon>
+      </router-link>
+      <v-icon small class="mr-2" @click="deleteRecord(item.id)"
+        >mdi-delete</v-icon
+      >
+    </template>
+  </v-data-table>
+</template>
+
+<script lang="ts">
+export default {
+  props: ["editPage", "headers", "records"]
+};
+</script>
+
+<style scoped lang="scss">
+a {
+  text-decoration: none;
+}
+</style>
