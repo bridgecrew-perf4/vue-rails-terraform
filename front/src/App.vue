45ca9900297f68c5f0a32b0e32363f668721e980
naokivandit <n.i.vandit0924@gmail.com>
2021-02-03 03:20:04 +0900

    fix: app vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,25 +1,64 @@
 <template>
   <v-app>
     <Header />
-
     <v-main>
+      <SideNav />
       <router-view />
     </v-main>
-
     <Footer />
   </v-app>
 </template>
 
-<script>
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+<script lang="ts">
+import firebase from "firebase/app";
+import { mapActions, mapGetters } from "vuex";
+
+import Header from "./components/organisms/Header.vue";
+import SideNav from "./components/organisms/SideNav.vue";
+import Footer from "./components/organisms/Footer.vue";
 export default {
   name: "App",
   components: {
     Header,
+    SideNav,
     Footer
+  },
+  beforeCreate() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setLoginUser(user);
+        if (this.$router.currentRoute.name === "home")
+          this.$router.push({ name: "weights" });
+      } else {
+        this.deleteLoginUser();
+        this.$router.push({ name: "home" });
+      }
+    });
+  },
+  created() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setLoginUser(user);
+        if (this.$router.currentRoute.name === "home")
+          this.$router.push({ name: "weights" });
+      } else {
+        this.deleteLoginUser();
+        this.$router.push({ name: "home" });
+      }
+    });
+   
+  },
+  methods: {
+    ...mapActions(["setLoginUser", "logout", "deleteLoginUser"])
+  },
+  computed: {
+    ...mapGetters(["uid"])
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+a {
+  text-decoration: none;
+}
+</style>
