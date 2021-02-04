ed9310555e0107cb1853cd6abc3e33d0b1fd074a
naoki ishii <30743722+naokivandit@users.noreply.github.com>
2021-02-04 21:46:42 +0900

    Fix/bug google login (#7)
    * feature: add pull request template
    
    * fix: main js

--- front/src/main.js
+++ front/src/main.js
@@ -1,17 +1,29 @@
-import Vue from "vue";
-import axios from "axios";
-import VueAxios from "vue-axios";
-
-import router from "./router";
 import App from "./App.vue";
+import axios from "axios";
+import firebase from "firebase";
+import router from "./router";
+import store from "./store";
+import Vue from "vue";
+import VueAxios from "vue-axios";
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueAxios, axios);
 
+const config = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID
+};
+firebase.initializeApp(config);
+
 new Vue({
   router,
+  store,
   vuetify,
   render: h => h(App)
 }).$mount("#app");
