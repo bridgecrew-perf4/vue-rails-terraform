474a0888db1abeef3ed98367b2c90f9d515bb1ad
naokivandit <n.i.vandit0924@gmail.com>
2021-02-06 18:25:05 +0900

    feature: store js

---
+++ front/src/store/index.js
@@ -0,0 +1,52 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import firebase from "firebase";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    login_user: null,
+    drawer: false,
+    uid: null
+  },
+  mutations: {
+    setLoginUser(state, user) {
+      state.login_user = user;
+      state.uid = user.uid;
+    },
+    deleteLoginUser(state) {
+      state.login_user = null;
+      state.uid = null;
+    },
+    toggleSideMenu(state) {
+      state.drawer = !state.drawer;
+    }
+  },
+  actions: {
+    setLoginUser({ commit }, user) {
+      commit("setLoginUser", user);
+    },
+    setUserID({ commit }, user) {
+      commit("setUserID", user);
+    },
+    login() {
+      const google_auth_provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(google_auth_provider);
+    },
+    logout() {
+      firebase.auth().signOut();
+    },
+    deleteLoginUser({ commit }) {
+      commit("deleteLoginUser");
+    },
+    toggleSideMenu({ commit }) {
+      commit("toggleSideMenu");
+    }
+  },
+  getters: {
+    userName: state => (state.login_user ? state.login_user.displayName : ""),
+    photoURL: state => (state.login_user ? state.login_user.photoURL : ""),
+    uid: state => (state.login_user ? state.login_user.uid : "")
+  },
+});
